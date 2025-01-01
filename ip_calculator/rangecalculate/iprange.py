572156034ac6ec42a8ac759e183f69c86d5e4ba1
0manthan <168362863+0manthan@users.noreply.github.com>
2025-01-01 11:36:34 +0530

    Add files via upload

---
+++ ip_calculator/rangecalculate/iprange.py
@@ -0,0 +1,5 @@
+from bccal import broadcastadd
+from nkcal import networkadd
+
+def iprange(uservar,uservar1,uservar2,uservar3):
+    print(f"usable range  is {uservar}.{uservar1}.{uservar2}.1 to {uservar}.{uservar1}.{uservar2}.{uservar3}")
